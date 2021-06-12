980214c0054bed54d0a23731167bcbc2dfca9136
Rommy <ceritarommy@gmail.com>
2021-06-12 11:38:26 +0700

    domain grabber

--- domain.py
+++ domain.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import re
 import urllib3
@@ -15,24 +16,44 @@ Github: @ceritarommy
 Instagram: @rommymaul
     """)
 
+def checkOutput():
+
+    global domain_file
+
+    # remove old output if exist
+    if os.path.exists(domain_file):
+        os.remove(domain_file)
+
 def grabDomain(pages):
 
     global domain_file
 
     try:
+
+        # mirror list
         mirror = [
             "https://zone-d.org/mirror/archive/",
-            "https://mirror-h.org/archive/page/"
+            "https://mirror-h.org/archive/page/",
+            "https://zone-xsec.com/archive/page="
         ]
+
+        # total mirror list
         total_mirror = len(mirror)
         
         for x in range(0, total_mirror):
+
+            # requests
             url = f"{mirror[x]}{pages}"
             header = {"User-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"}
             response = requests.get(url, headers=header, timeout=5, verify=False)
+
+            # regex
             grab_zone_d = re.findall('<td class=special style="width: 10px;padding:0 5px;"><td>(.*?)<td>', response.text)
             grab_mirror_h = re.findall('<td style="word-break: break-word;white-space: normal;min-width: 300px;"><?a?.+\>(.*?)</a></td>', response.text)
+            grab_zxsec = re.findall('''            <td></td>
+            <td>(.*?)</td>''', response.text)
 
+            # grab from zone-d
             for zone_d in grab_zone_d:
                 zone_d = zone_d.split("/")
                 save = open(domain_file, "a")
@@ -40,17 +61,27 @@ def grabDomain(pages):
                 save.close()
                 print(zone_d[0])
 
+            # grab from mirror-h
             for mirror_h in grab_mirror_h:
                 mirror_h = mirror_h.split("/")
                 save = open(domain_file, "a")
                 save.write(f"{mirror_h[2]}\n")
                 save.close()
                 print(mirror_h[2])
+            
+            # grab from zone-xsec
+            for zxsec in grab_zxsec:
+                zxsec = zxsec.split("/")
+                save = open(domain_file, "a")
+                save.write(f"{zxsec[0]}\n")
+                save.close()
+                print(zxsec[0])
 
     except:
         pass
 
 banner()
+checkOutput()
 
 total_pages = int(input("Total pages: "))
 print("\n")
